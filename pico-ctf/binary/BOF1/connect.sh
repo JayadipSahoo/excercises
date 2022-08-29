3a61d138237a86a77bff7b19fc67417b7c5ee22d
iyamroshan <iyamroshan0@gmail.com>
2022-08-29 08:27:26 +0545

    add: binary + reverse eng.. of pico-ctf

---
+++ pico-ctf/binary/BOF1/connect.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+command nc saturn.picoctf.net 57089
---
+++ pico-ctf/reverse/filerun1/soln.sh
@@ -0,0 +1,4 @@
+#!usr/bin/env bash 
+command wget https://artifacts.picoctf.net/c/310/run
+command chmod +x run
+#flag picoCTF{U51N6_Y0Ur_F1r57_F113_47cf2b7b}
